43148e93e64fc2751850c48bb39d2ddcaec6096b
anavegaa <110676643+anavegaa@users.noreply.github.com>
2025-06-26 18:19:54 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
-    val email = "testeo1@gmail.com"
+    val email = "testeo12@gmail.com"
     val password = "testeo1234"
 }
